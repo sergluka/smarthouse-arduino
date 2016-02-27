1357c8e4ad0d6b92375ed94aa349ad0f95998801
Sergey Lukashevich <sergey.lukashevi4@gmail.com>
2016-02-27 23:28:00 +0200

    Kitchen LEDs improving
    Improve logging;
    Add VER1 parsing (No handling for now)

--- Kitchen/Kitchen.ino
+++ Kitchen/Kitchen.ino
@@ -1,16 +1,16 @@
 #include <Arduino.h>
 
-#include <stdlib.h>
-#include <errno.h>
-
 #include <MySensor.h>
 #include <EEPROM.h>
-#include <Button.h>
-#include <SimpleTimer.h>
+#include <NewButton.h>
 #include <LEDFader.h>
 #include <Logging.h>
 
+#include "Types.h"
+#include "Parsing.h"
+
 #define VERSION "0.2"
+#define STORAGE_VERSION 1
 
 #define PIN_IN_RED  3
 #define PIN_IN_GREEN  5
@@ -23,33 +23,24 @@
 #define MS_RF24_PA_LEVEL    RF24_PA_MAX
 
 #define FADE_TIME_MS    3000
-#define HEARTBEAT_PERIOD_MS  60 * 60 * 1000U
 
 #define SERIAL_SPEED    115200U
 
 #define MS_NODE_ID         3
-#define MS_SENSOR_LEDS_ID  0
-
-void on_heartbeat_timer();
-
-struct RGBW {
-    byte R;
-    byte G;
-    byte B;
-    byte W;
-} __attribute__((packed));
+#define MS_SENSOR_COLOR_LEDS_ID  0
+#define MS_SENSOR_WHITE_LED_ID   1
 
 struct Storage
 {
-    bool has_values;
+    byte version;
     RGBW color;
 }  __attribute__((packed));
 
-Storage storage = {false, {0, 0, 0, 0}};
+Storage storage = {STORAGE_VERSION, {0, 0, 0, 0}};
 
 bool leds_on = false;
 
-Button button(PIN_IN_BUTTON);
+NewButton button(PIN_IN_BUTTON);
 LEDFader ledW;
 LEDFader ledR;
 LEDFader ledG;
@@ -57,13 +48,12 @@ LEDFader ledB;
 
 MyTransportNRF24 transport(MS_RF24_CE_PIN, MS_RF24_CS_PIN, MS_RF24_PA_LEVEL);
 MySensor gw(transport);
-MyMessage msgLedStatus(MS_SENSOR_LEDS_ID, S_LIGHT);
-
-SimpleTimer hearbeat_timer;
+MyMessage msgColorLedStatus(MS_SENSOR_COLOR_LEDS_ID, S_LIGHT);
+MyMessage msgWhiteLedStatus(MS_SENSOR_WHITE_LED_ID, S_LIGHT);
 
 void setup()
 {
-    Serial.begin(SERIAL_SPEED);
+    LOGGING_SETUP(SERIAL_SPEED);
     LOG_DEBUG("Setup has been started.")
 
     LOG_DEBUG("EEPROM restore...");
@@ -76,28 +66,23 @@ void setup()
     ledW = LEDFader(PIN_IN_WHITE);
 
     LOG_INFO("Button setup...");
-    button.init();
-    button.set_minimum_gap(20);
-    button.set_maximum_gap(1000);
-    button.on_press(on_btn_press);
+    button.setup();
+    button.on_short_release(on_btn_short_release);
 
     LOG_INFO("MySensors setup...");
     gw.begin(on_message, MS_NODE_ID);
     gw.sendSketchInfo("KitchenLEDs", VERSION);
-    gw.present(MS_SENSOR_LEDS_ID, S_RGB_LIGHT, "Color LEDs");
-    gw.present(MS_SENSOR_LEDS_ID, S_LIGHT, "White LED");
+    gw.present(MS_SENSOR_COLOR_LEDS_ID, S_RGB_LIGHT, "Color LEDs");
+    gw.present(MS_SENSOR_WHITE_LED_ID, S_LIGHT, "White LED");
 
     switch_leds_off();
-
-    hearbeat_timer.setInterval(HEARTBEAT_PERIOD_MS, on_heartbeat_timer);
-    on_heartbeat_timer();
 
     LOG_INFO("Setup done");
 }
 
 void loop()
 {
-    button.init();
+    button.process();
 
     gw.process();
 
@@ -105,11 +90,9 @@ void loop()
     ledG.update();
     ledB.update();
     ledW.update();
-
-    hearbeat_timer.run();
 }
 
-void on_btn_press()
+void on_btn_short_release()
 {
     leds_on = !leds_on;
 
@@ -123,56 +106,73 @@ void on_btn_press()
     }
 }
 
-void on_heartbeat_timer()
-{
-    gw.sendBatteryLevel(100);
-}
-
 void on_message(const MyMessage & message)
 {
-    if (message.sensor != MS_SENSOR_LEDS_ID) {
+    if (message.sensor != MS_SENSOR_COLOR_LEDS_ID && message.sensor != MS_SENSOR_WHITE_LED_ID) {
         LOG_ERROR("Got message from unexpected sensor: (sensor=%d, type=%d)", message.sensor, message.type);
         return;
     }
 
-    if (message.type == V_RGBW) {
-        on_message_set_rgbw(message);
+    if (message.type == V_VAR1) {
+        on_message_var1(message);
     }
-    else if (message.type == V_STATUS) {
-        on_message_set_status(message);
-    }
+//    else if (message.type == V_STATUS) {
+//        on_message_set_status(message);
+//    }
+//    else if (message.type == V_VAR1) {
+//        on_message_set_status(message);
+//    }
     else {
         LOG_ERROR("Got message with unexpected type: (sensor=%d, type=%d)", message.sensor, message.type);
     }
 }
 
-void on_message_set_rgbw(const MyMessage & message)
+void on_message_var1(const MyMessage & message)
 {
-    const char * rgb_string = message.getString();
-    LOG_DEBUG("=> Message: sensor=%d, type=%d, value=%s", message.sensor, message.type,
-              rgb_string);
-    RGBW rgbw = string_to_rgbw(rgb_string);
+    const char * payload = message.getString();
+    LOG_DEBUG("=> Message: sensor=%d, type=%d, data=%s", message.sensor, message.type, payload);
 
-    fade_leds(rgbw.R, rgbw.G, rgbw.B, rgbw.W);
-    save_led_values(rgbw);
-}
-
-void on_message_set_status(const MyMessage & message)
-{
-    bool status = message.getBool();
-    LOG_DEBUG("=> Message: sensor=%d, type=%d, value=%d", message.sensor, message.type, status);
-
-    if (status) {
-        restore_leds();
+    LedType led_type;
+    if (message.sensor == MS_SENSOR_COLOR_LEDS_ID) {
+        led_type = LedType::Color;
+    }
+    else if (message.sensor == MS_SENSOR_WHITE_LED_ID) {
+        led_type = LedType::White;
     }
     else {
-        switch_leds_off();
+        LOG_ERROR("Unknown sensor");
+        return;
     }
+
+    CommandData data = parse_command(payload, led_type);
 }
+
+//void on_message_set_rgbw(const MyMessage & message)
+//{
+//    const char * rgb_string = message.getString();
+//    LOG_DEBUG("=> Message: sensor=%d, type=%d, value=%s", message.sensor, message.type,
+//              rgb_string);
+//    RGBW rgbw = string_to_rgbw(rgb_string);
+//
+//    fade_leds(rgbw.R, rgbw.G, rgbw.B, rgbw.W);
+//    save_led_values(rgbw);
+//}
+
+//void on_message_set_status(const MyMessage & message)
+//{
+//    bool status = message.getBool();
+//    LOG_DEBUG("=> Message: sensor=%d, type=%d, value=%d", message.sensor, message.type, status);
+//
+//    if (status) {
+//        restore_leds();
+//    }
+//    else {
+//        switch_leds_off();
+//    }
+//}
 
 void save_led_values(const RGBW & rgbw)
 {
-    storage.has_values = true;
     storage.color = rgbw;
     EEPROM.put(0, storage);
 }
@@ -180,7 +180,7 @@ void save_led_values(const RGBW & rgbw)
 void load_led_values()
 {
     EEPROM.get(0, storage);
-    if (!storage.has_values) {
+    if (storage.version != STORAGE_VERSION) {
         storage.color.R = 255;
         storage.color.G = 255;
         storage.color.B = 255;
@@ -192,21 +192,24 @@ void restore_leds()
 {
     fade_leds(storage.color.R, storage.color.G, storage.color.B, 255);
     leds_on = true;
-    send(msgLedStatus.set(1));
+    send(msgColorLedStatus.set(1));
+    send(msgWhiteLedStatus.set(1));
 }
 
-void switch_colorleds_on(byte R, byte G, byte B, byte W)
+void switch_leds_on(byte R, byte G, byte B, byte W)
 {
     fade_leds(R, G, B, W);
     leds_on = true;
-    send(msgLedStatus.set(1));
+    send(msgColorLedStatus.set(1));
+    send(msgWhiteLedStatus.set(1));
 }
 
 void switch_leds_off()
 {
     fade_leds(0, 0, 0, 0);
     leds_on = false;
-    send(msgLedStatus.set(0));
+    send(msgColorLedStatus.set(0));
+    send(msgWhiteLedStatus.set(0));
 }
 
 void fade_leds(byte R, byte G, byte B, byte W)
@@ -223,49 +226,4 @@ void send(MyMessage & message)
     if (!gw.send(message)) {
         LOG_ERROR("Message (sensor=%d, type=%d) doesn't reach a next node", message.sensor, message.type);
     }
-}
-
-RGBW string_to_rgbw(const char * str)
-{
-    RGBW result = {0, 0, 0, 0};
-
-    if (!str) {
-        LOG_ERROR("RGBW string is NULL");
-        return {0, 0, 0, 0};
-    }
-
-    size_t len = strlen(str);
-    if (len != 4*2) {
-        LOG_ERROR("Unexpected length of RGBW string: %d", len);
-        return {0, 0, 0, 0};
-    }
-
-    char buffer[9];
-    strncpy(buffer, str, sizeof(buffer));
-
-    result.W = strtol(buffer+6, nullptr, 16);
-    if (errno != 0) {
-        LOG_ERROR("Fail to parse W in string: %s", str);
-        return {0, 0, 0, 0};
-    }
-    buffer[6] = '\0';
-    result.B = strtol(buffer+4, nullptr, 16);
-    if (errno != 0) {
-        LOG_ERROR("Fail to parse B in string: %s", str);
-        return {0, 0, 0, 0};
-    }
-    buffer[4] = '\0';
-    result.G = strtol(buffer+2, nullptr, 16);
-    if (errno != 0) {
-        LOG_ERROR("Fail to parse G in string: %s", str);
-        return {0, 0, 0, 0};
-    }
-    buffer[2] = '\0';
-    result.R = strtol(buffer, nullptr, 16);
-    if (errno != 0) {
-        LOG_ERROR("Fail to parse R in string: %s", str);
-        return {0, 0, 0, 0};
-    }
-
-    return result;
 }
--- libraries/Logging/Logging.h
+++ libraries/Logging/Logging.h
@@ -3,8 +3,12 @@
 
 #include <Arduino.h>
 
-void log(const __FlashStringHelper *fmt, ... ){
+#include <string.h>
+
+void log(const __FlashStringHelper *fmt, ...)
+{
     static char buf[128];
+
     va_list args;
     va_start (args, fmt);
     vsnprintf_P(buf, sizeof(buf), (const char *)fmt, args); // progmem for AVR
@@ -13,8 +17,12 @@ void log(const __FlashStringHelper *fmt, ... ){
     Serial.flush();
 }
 
-#define LOG_DEBUG(_msg, ...) log(F("[DEBUG] " _msg) , ##__VA_ARGS__);
-#define LOG_INFO(_msg, ...)  log(F("[INFO] "  _msg) , ##__VA_ARGS__);
-#define LOG_ERROR(_msg, ...) log(F("[ERROR] " _msg) , ##__VA_ARGS__);
+#define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+
+#define LOGGING_SETUP(_speed) Serial.begin(_speed)
+#define LOG_DEBUG(_msg, ...) log(F("[DEBUG] " _msg " [%s:%d]") , ##__VA_ARGS__, __FILENAME__, __LINE__);
+#define LOG_INFO(_msg, ...)  log(F("[INFO] "  _msg " [%s:%d]") , ##__VA_ARGS__, __FILENAME__, __LINE__);
+#define LOG_ERROR(_msg, ...) log(F("[ERROR] " _msg " [%s:%d]") , ##__VA_ARGS__, __FILENAME__, __LINE__);
 
 #endif // _LOGGING_
+
