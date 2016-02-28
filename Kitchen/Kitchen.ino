9ae6d2ab0baec2a829ac2dfeb208f797e85fc514
Sergey Lukashevich <sergey.lukashevi4@gmail.com>
2016-02-28 16:30:02 +0200

    Kitchen LEDs improving
    Handle on, off and limit;
    Extract LEDs functions.

--- Kitchen/Kitchen.ino
+++ Kitchen/Kitchen.ino
@@ -3,26 +3,20 @@
 #include <MySensor.h>
 #include <EEPROM.h>
 #include <NewButton.h>
-#include <LEDFader.h>
 #include <Logging.h>
 
 #include "Types.h"
+#include "LEDs.h"
 #include "Parsing.h"
 
 #define VERSION "0.2"
 #define STORAGE_VERSION 1
 
-#define PIN_IN_RED  3
-#define PIN_IN_GREEN  5
-#define PIN_IN_BLUE  6
 #define PIN_IN_BUTTON  7
-#define PIN_IN_WHITE  9
 
 #define MS_RF24_CE_PIN      4       //<-- NOTE!!! changed, the default is 9                                                                                                                                     ░
 #define MS_RF24_CS_PIN      10
 #define MS_RF24_PA_LEVEL    RF24_PA_MAX
-
-#define FADE_TIME_MS    3000
 
 #define SERIAL_SPEED    115200U
 
@@ -38,13 +32,7 @@ struct Storage
 
 Storage storage = {STORAGE_VERSION, {0, 0, 0, 0}};
 
-bool leds_on = false;
-
 NewButton button(PIN_IN_BUTTON);
-LEDFader ledW;
-LEDFader ledR;
-LEDFader ledG;
-LEDFader ledB;
 
 MyTransportNRF24 transport(MS_RF24_CE_PIN, MS_RF24_CS_PIN, MS_RF24_PA_LEVEL);
 MySensor gw(transport);
@@ -58,12 +46,6 @@ void setup()
 
     LOG_DEBUG("EEPROM restore...");
     load_led_values();
-
-    LOG_INFO("Leds setup...");
-    ledR = LEDFader(PIN_IN_RED);
-    ledG = LEDFader(PIN_IN_GREEN);
-    ledB = LEDFader(PIN_IN_BLUE);
-    ledW = LEDFader(PIN_IN_WHITE);
 
     LOG_INFO("Button setup...");
     button.setup();
@@ -86,23 +68,18 @@ void loop()
 
     gw.process();
 
-    ledR.update();
-    ledG.update();
-    ledB.update();
-    ledW.update();
+    leds_process();
 }
 
 void on_btn_short_release()
 {
-    leds_on = !leds_on;
-
     LOG_DEBUG("Button press");
 
-    if (leds_on) {
-        restore_leds();
+    if (is_light_on()) {
+        switch_leds_off();
     }
     else {
-        switch_leds_off();
+        restore_leds();
     }
 }
 
@@ -116,12 +93,6 @@ void on_message(const MyMessage & message)
     if (message.type == V_VAR1) {
         on_message_var1(message);
     }
-//    else if (message.type == V_STATUS) {
-//        on_message_set_status(message);
-//    }
-//    else if (message.type == V_VAR1) {
-//        on_message_set_status(message);
-//    }
     else {
         LOG_ERROR("Got message with unexpected type: (sensor=%d, type=%d)", message.sensor, message.type);
     }
@@ -145,31 +116,57 @@ void on_message_var1(const MyMessage & message)
     }
 
     CommandData data = parse_command(payload, led_type);
+    switch (data.command) {
+    case Command::LIGHT_OFF:
+        on_message_light_off(message.sensor);
+        break;
+    case Command::LIGHT_ON:
+        on_message_light_on(message.sensor);
+        break;
+    case Command::LIGHT_SET_LIMIT:
+        on_message_set_limit(message.sensor, data.limit);
+        break;
+    default:
+        break;
+    }
 }
 
-//void on_message_set_rgbw(const MyMessage & message)
-//{
-//    const char * rgb_string = message.getString();
-//    LOG_DEBUG("=> Message: sensor=%d, type=%d, value=%s", message.sensor, message.type,
-//              rgb_string);
-//    RGBW rgbw = string_to_rgbw(rgb_string);
-//
-//    fade_leds(rgbw.R, rgbw.G, rgbw.B, rgbw.W);
-//    save_led_values(rgbw);
-//}
+void on_message_light_off(uint8_t sensor)
+{
+    if (sensor == MS_SENSOR_COLOR_LEDS_ID) {
+        fade_color_leds(0, 0, 0);
+    }
+    else if (sensor == MS_SENSOR_WHITE_LED_ID) {
+        fade_white_led(0);
+    }
+}
 
-//void on_message_set_status(const MyMessage & message)
-//{
-//    bool status = message.getBool();
-//    LOG_DEBUG("=> Message: sensor=%d, type=%d, value=%d", message.sensor, message.type, status);
-//
-//    if (status) {
-//        restore_leds();
-//    }
-//    else {
-//        switch_leds_off();
-//    }
-//}
+void on_message_light_on(uint8_t sensor)
+{
+    if (sensor == MS_SENSOR_COLOR_LEDS_ID) {
+        fade_color_leds(storage.color.R, storage.color.G, storage.color.B);
+    }
+    else if (sensor == MS_SENSOR_WHITE_LED_ID) {
+        fade_white_led(storage.color.W);
+    }
+
+}
+
+void on_message_set_limit(uint8_t sensor, RGBW rgbw)
+{
+    if (sensor == MS_SENSOR_COLOR_LEDS_ID) {
+        storage.color.R = rgbw.R;
+        storage.color.G = rgbw.G;
+        storage.color.B = rgbw.B;
+        fade_color_leds(storage.color.R, storage.color.G, storage.color.B);
+    }
+    else if (sensor == MS_SENSOR_WHITE_LED_ID) {
+        storage.color.W = rgbw.W;
+        fade_white_led(storage.color.W);
+    }
+
+    save_led_values(storage.color);
+}
 
 void save_led_values(const RGBW & rgbw)
 {
@@ -190,16 +187,7 @@ void load_led_values()
 
 void restore_leds()
 {
-    fade_leds(storage.color.R, storage.color.G, storage.color.B, 255);
-    leds_on = true;
-    send(msgColorLedStatus.set(1));
-    send(msgWhiteLedStatus.set(1));
-}
-
-void switch_leds_on(byte R, byte G, byte B, byte W)
-{
-    fade_leds(R, G, B, W);
-    leds_on = true;
+    fade_leds(storage.color.R, storage.color.G, storage.color.B, storage.color.W);
     send(msgColorLedStatus.set(1));
     send(msgWhiteLedStatus.set(1));
 }
@@ -207,18 +195,8 @@ void switch_leds_on(byte R, byte G, byte B, byte W)
 void switch_leds_off()
 {
     fade_leds(0, 0, 0, 0);
-    leds_on = false;
     send(msgColorLedStatus.set(0));
     send(msgWhiteLedStatus.set(0));
-}
-
-void fade_leds(byte R, byte G, byte B, byte W)
-{
-    ledR.fade(R, FADE_TIME_MS);
-    ledG.fade(G, FADE_TIME_MS);
-    ledB.fade(B, FADE_TIME_MS);
-    ledW.fade(W, FADE_TIME_MS);
-    LOG_INFO("LEDs are fading to R=0x%x, G=0x%x, B=0x%x, W=0x%x, time=%dms", R, G, B, W, FADE_TIME_MS);
 }
 
 void send(MyMessage & message)
